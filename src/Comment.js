2ebf628ef84aab34c7ad7ee52d01e854b40fcf45
Kacper Stroisz <kacperstroisz@wp.pl>
2018-05-01 21:09:55 +0200

    Add button remove and edit

--- src/Comment.js
+++ src/Comment.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
-const Comment = ({ text, votes, id, thumbUpComment, thumbDownComment }) =>
+const Comment = ({ text, votes, id, thumbUpComment, thumbDownComment, removeComment, editComment }) =>
     <li>
     {text} <span>votes: {votes}</span> 
     <button onClick={() => thumbUpComment(id)}>Thumb up</button>
     <button onClick={() => thumbDownComment(id)}>Thumb down</button>
+    <button onClick={() => removeComment(id)}>Remove</button>
+    <button onClick={() => editComment(id)}>Edit</button>
   </li>;
 
 export default Comment;
--- src/comments.js
+++ src/comments.js
@@ -19,14 +19,11 @@ function comments(state = [], action) {
         case EDIT_COMMENT:
             return state.map(comment => {
                 if (comment.id === action.id) {
-                    return Object.assign({}, comment, {
-                        text: action.text
-                    });
-
-                } else {
-                    return comment;
+                    return { ...comment, text: action.text }
                 }
-            })
+                return comment;
+            });
+
         case THUMB_UP_COMMENT:
             return state.map(comment => {
                 if (comment.id === action.id) {
@@ -34,6 +31,7 @@ function comments(state = [], action) {
                 }
                 return comment;
             });
+
         case THUMB_DOWN_COMMENT:
             return state.map(comment => {
                 if (comment.id === action.id) {
